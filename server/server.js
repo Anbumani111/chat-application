8d85efe2d49e3031a4b35efa70628750b6479616
Anbumani <v.anbumani3072003@gmail.com>
2026-04-13 21:11:43 +0530

    all changes

--- server/server.js
+++ server/server.js
@@ -41,9 +41,8 @@ app.use("/api/messages",messageRouter);
 
 await connectDB(); // connect to MongoDB
 
-if(process.env.NODE_ENV!=="production"){
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
-}
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+
 
 export default server;
